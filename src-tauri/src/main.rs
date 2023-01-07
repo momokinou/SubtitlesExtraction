2260484058c34c29fe880c910c42bd3673218e89
momokinou <moriceau.q@gmail.com>
2023-01-07 02:43:06 +0100

    feat: Sort presque fonctionnel

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,9 @@
     windows_subsystem = "windows"
 )]
 
-use std::any::Any;
 use std::fs;
 use std::path::Path;
+use tauri::regex::Regex;
 use tauri_plugin_store::PluginBuilder;
 
 //Thanks to Golden_Water. Go see his bilibili profile. (space.bilibili.com/41925356)
@@ -23,21 +23,54 @@ enum Entry {
     },
 }
 
+fn modify_dirs(entry: &mut Entry) {
+    match entry {
+        Entry::Dir { children, .. } => {
+            for child in children {
+                modify_dirs(child);
+            }
+
+            if let Entry::Dir { children, .. } = entry {
+                children.sort();
+            }
+        }
+        Entry::File { .. } => {}
+    }
+}
+
 #[tauri::command]
 fn list_subdirectories(dir: &Path, id: i64) -> String {
     let mut idd: i64 = id;
     let tree: Vec<Entry> = list_subdirectorie(dir, &mut idd);
+    // for entry in &tree {
+    //     // println!("{:?}", entry);
+    // }
     let ret_json = serde_json::to_string_pretty(&tree).unwrap();
     ret_json
+}
+
+trait FileText {
+    fn file_text(&self) -> &str;
+}
+
+impl FileText for Entry {
+    fn file_text(&self) -> &str {
+        match self {
+            Entry::Dir { text, .. } => text,
+            Entry::File { text, .. } => text,
+        }
+    }
 }
 
 //Thanks to Golden_Water. Go see his bilibili profile. (space.bilibili.com/41925356)
 fn list_subdirectorie(dir: &Path, id: &mut i64) -> Vec<Entry> {
     let mut sub_entries = vec![];
+    let re = Regex::new(r"(\d+)").unwrap();
 
     for entry in std::fs::read_dir(dir).unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
+        // println!("{:?}", path);
         let name = path.file_name().unwrap().to_str().unwrap().to_string();
 
         if path.is_dir() {
@@ -57,7 +90,17 @@ fn list_subdirectorie(dir: &Path, id: &mut i64) -> Vec<Entry> {
             });
         }
     }
-    sub_entries.sort_by_key(|d| d.type_id());
+    sub_entries.sort_by(|a, b| {
+        let a_num: i32 = re
+            .captures(a.file_text())
+            .and_then(|caps| caps.get(1).map(|m| m.as_str().parse().unwrap()))
+            .unwrap_or(std::i32::MAX);
+        let b_num: i32 = re
+            .captures(b.file_text())
+            .and_then(|caps| caps.get(1).map(|m| m.as_str().parse().unwrap()))
+            .unwrap_or(std::i32::MAX);
+        a_num.cmp(&b_num)
+    });
     sub_entries
 }
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import { Theme } from "carbon-components-svelte";
   import "carbon-components-svelte/css/all.css";
+  import FileTreeView from "../lib/components/FileTreeView.svelte";
 
   console.log("index");
 </script>
 
-<Theme />
+<FileTreeView />
